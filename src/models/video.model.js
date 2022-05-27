1fe3a9885541800d191b1955153b7b05340b29d4
Jefferson <jefry04@gmail.com>
2022-05-27 18:19:33 -0500

    create users collections with validation

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -4,7 +4,6 @@ const { Schema, model } = require("mongoose");
 const videoSchema = new Schema(
   {
     title: String,
-    userId: Number,
     description: String,
     url: String,
     visits: Number,
---
+++ src/routes/user.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const userControllers = require("../controllers/user.controller");
+
+router.route("/").get(userControllers.list);
+router.route("/:userId").get(userControllers.show);
+router.route("/").post(userControllers.create);
+router.route("/:userId").put(userControllers.update);
+router.route("/:userId").delete(userControllers.destroy);
+
+module.exports =router;
