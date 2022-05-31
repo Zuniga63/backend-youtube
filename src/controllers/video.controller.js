e1dd14356e6ba17dd5108e8f80d5b0cb65d3e8fb
Jefferson <jefry04@gmail.com>
2022-05-31 18:01:24 -0500

    change user and video controller to async await syntaxis

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,29 +1,27 @@
 const Video = require("../models/video.model");
-const User = require ("../models/user.model");
+const User = require("../models/user.model");
 
 module.exports = {
-
-  list(req, res) {
-    Video.find()
-      .then((videos) => {
-        res.status(200).json({ message: "Videos found", data: videos });
-      })
-      .catch((err) => {
-        res.status(404).json({ message: "Videos nor found" });
-      });
+  async list(req, res) {
+    try {
+      const videos = await Video.find().populate("userId", "name email avatar");
+      res.status(200).json({ message: "Videos found", data: videos });
+    } catch (err) {
+      res.status(404).json({ message: "Videos nor found" });
+    }
   },
 
-  show(req, res) {
-    const { videoId } = req.params;
-
-    Video.findById(videoId)
-      .populate("userId", "name email")
-      .then((video) => {
-        res.status(200).json({ message: "Video found", data: video });
-      })
-      .catch((err) => {
-        res.status(404).json({ message: "Video not found" });
-      });
+  async show(req, res) {
+    try {
+      const { videoId } = req.params;
+      const video = await Video.findById(videoId).populate(
+        "userId",
+        "name email avatar"
+      );
+      res.status(200).json({ message: "Video found", data: video });
+    } catch (err) {
+      res.status(404).json({ message: "Video not found", data: err });
+    }
   },
 
   async create(req, res) {
@@ -35,37 +33,33 @@ module.exports = {
       }
       const video = await Video.create({ ...req.body });
       res.status(201).json(video);
-
-    }catch (err){
+    } catch (err) {
       res.status(400).json(err);
     }
   },
 
-  update(req, res) {
-    const { videoId } = req.params;
+  async update(req, res) {
+    try {
+      const { videoId } = req.params;
 
-    Video.findByIdAndUpdate(videoId, req.body, { new: true })
-      .then((video) => {
-        res.status(200).json({ message: "video updated", data: video });
-      })
-      .catch((err) => {
-        res
-          .status(400)
-          .json({ message: "Video could not be updated", data: err });
+      const video = await Video.findByIdAndUpdate(videoId, req.body, {
+        new: true,
       });
+      res.status(200).json({ message: "video updated", data: video });
+    } catch (err) {
+      res
+        .status(400)
+        .json({ message: "Video could not be updated", data: err });
+    }
   },
 
-  destroy(req, res) {
-    const { videoId } = req.params;
-
-    Video.findByIdAndDelete(videoId)
-      .then((video) => {
-        res.status(200).json({ message: "video deleted", data: video });
-      })
-      .catch((err) => {
-        res
-          .status(400)
-          .json({ message: "Video could not be deleted", data: err });
-      });
+  async destroy(req, res) {
+    try {
+      const { videoId } = req.params;
+      const video = await Video.findByIdAndDelete(videoId);
+      res.status(200).json({ message: "video deleted", data: video });
+    } catch (err){
+      res.status(400).json({ message: "Video could not be deleted", data: err });
+    }
   },
 };
