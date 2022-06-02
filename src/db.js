a347489b45570da93eaf7766b1c5d016b99fe863
Jefferson <jefry04@gmail.com>
2022-06-02 10:40:25 -0500

    create enviroment variables

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connect() {
-  mongoose.connect("mongodb://localhost:27017/youtube", {
+  mongoose.connect(process.env.MONGO_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
