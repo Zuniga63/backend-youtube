72fafc2424813a8e18af5c9d9cb0d4566ebba170
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-06-02 15:37:10 -0500

    update enviroment variables

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const { connect } = require("./src/db");
 const videoRouter = require("./src/routes/video");
 const userRouter = require("./src/routes/user");
 const comentRouter = require("./src/routes/comment");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.APP_PORT;
+const host = process.env.APP_URL;
 connect();
 
 app.use(express.json());
@@ -18,5 +19,5 @@ app.use("/users", userRouter);
 app.use("/comments", comentRouter);
 
 app.listen(port, () => {
-  console.log(`Successfully run in port: http:localhost:${port}`);
+  console.log(`App running in ${host}:${port}`);
 });
